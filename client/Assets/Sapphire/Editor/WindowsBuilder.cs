de1e550efbd5c72d8c55773ee8046ade868077e4
pabang0620 <pabang0620@users.noreply.github.com>
2026-09-20 21:32:18 +0900

    chore: Rebuild Windows standalone to include latest map changes

---
+++ client/Assets/Sapphire/Editor/WindowsBuilder.cs
@@ -0,0 +1,19 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class WindowsBuilder
+{
+    public static void Build()
+    {
+        string[] scenes = new string[] {
+            "Assets/Sapphire/Scenes/Login.unity",
+            "Assets/Sapphire/Scenes/CharacterSelect.unity",
+            "Assets/Sapphire/Scenes/CharacterCreate.unity",
+            "Assets/Sapphire/Scenes/VillageHub.unity",
+            "Assets/Sapphire/Scenes/SlimeKingdom.unity"
+        };
+        
+        BuildPipeline.BuildPlayer(scenes, "builds/Windows/SapphireRPG.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+        Debug.Log("Windows Build Successful!");
+    }
+}
